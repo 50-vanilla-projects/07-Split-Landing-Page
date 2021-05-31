d334fb3764958d417255064f4cb6acd7aca82aa1
terrifricker <terrifricker@yahoo.com>
2021-05-30 23:02:44 -0700

    js: add functionality - left or right side grow to 3/4 width on hover.

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
+const body = document.querySelector("body");
 const left = document.querySelector(".left-hand-side");
 const right = document.querySelector(".right-hand-side");
+
+body.addEventListener('mousemove', () => {
+    left.addEventListener('mouseover', (event) => {
+        left.style.flex=3;   
+    });
+    left.addEventListener('mouseleave', () => {
+        left.style.flex = 1;
+    });
+    
+    right.addEventListener('mouseover', () => {
+        right.style.flex = 3;
+    });
+    right.addEventListener('mouseleave', () => {
+        right.style.flex = 1;
+    });
+});
+
